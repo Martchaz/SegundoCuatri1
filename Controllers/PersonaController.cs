1b5b0b4b48101aec5b8a9248dfe309fc13c7dbff
Martin CHazarreta <martchaz84@gmail.com>
2025-08-20 20:33:55 -0300

    Agregue el metodo Post

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SegundoCuatri1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+
+
+        // POST api/<PersonaController>
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            List<Persona> personas = new List<Persona>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                personas.Add(new Persona
+                {
+                    Id = i,
+                    Nombre = $"Nombre{i}",
+                    Apellido = $"Apellido{i}",
+                    Edad = 20 + i,
+                    Email = $"nombre{i}@mail.com"
+                });
+            }
+
+            return Ok(personas);
+        }
+
+
+
+    }
+}
